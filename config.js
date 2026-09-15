cbf432bfbf3597bf484f826e9b3b7688a6cacadc
jcmdal <juliomendesinc@gmail.com>
2026-09-15 12:54:20 -0300

    chaveamento

--- config.js
+++ config.js
@@ -6,6 +6,6 @@
 // =============================================================================
 
 window.SUPABASE_CONFIG = {
-  url: "https://pxvujbjlqugedddnpkvw.supabase.co",      // ex: "https://seuprojeto.supabase.co"
-  anonKey: "sb_publishable_vwUzjL5TeGqOdcDkatY3iQ_jICONlvH",  // ex: "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9...."
+  url: "",      // ex: "https://seuprojeto.supabase.co"
+  anonKey: "",  // ex: "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9...."
 };
